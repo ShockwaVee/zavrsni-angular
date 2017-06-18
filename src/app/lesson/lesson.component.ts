7f9bfc08956ee079aa707761951b5d375e3d7cfb
Davor Tvoric <davor.tvoric@gmail.com>
2017-06-18 21:04:11 +0200

    Add a basic switcher for the questions

--- src/app/lesson/lesson.component.ts
+++ src/app/lesson/lesson.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Lesson } from './lesson.model';
 import { Question } from './question.model'; 
@@ -10,20 +13,42 @@ import { Question } from './question.model';
 })
 export class LessonComponent implements OnInit {
 	quiz_mode = false;
+	question_changed = new Subject<Question>();
+	subscription = new Subscription;
 	lesson: Lesson;
 	questions: Question[] = [
 		new Question('Prvo pitanje?', ['odgovor1', 'odgovor2', 'odgovor3', 'odgovor4'], 'odgovor3'),
 		new Question('Drugo pitanje?', ['odgovor1', 'odgovor2', 'odgovor3', 'odgovor4'], 'odgovor1'),
-	]
+	];
+	index: number;
+	current_question: Question;
 
   constructor() { }
 
   ngOnInit() {
   	this.lesson = new Lesson('Ovo je neki testni tekst lessona', this.questions);
+  	this.subscription = this.question_changed.subscribe((question: Question) => {
+  		this.current_question = question;
+  	});  	
+  	this.index = 0;
+		this.current_question = this.questions[0];
+
   }
 
   onQuizActivate(){
   	this.quiz_mode = true;
+  }
+
+  onSubmit(form: NgForm){
+  		if (this.index != this.questions.length-1){
+  			this.index++;
+  			this.question_changed.next(this.questions[this.index]);
+  		}
+  		else{
+  			this.quiz_mode = false;
+  			this.index = 0;
+  			this.question_changed.next(this.questions[this.index]);
+  		} 
   }
 
 }
